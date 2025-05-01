24d07a6162db0ba45ca9b59e0d2b376560937ef8
Restless <utrerajuandavid2008@gmail.com>
2025-05-01 19:00:00 -0400

    Update nsfw-cum.js

--- plugins/nsfw-cum.js
+++ plugins/nsfw-cum.js
@@ -31,17 +31,17 @@ if (!db.data.chats[m.chat].nsfw && m.isGroup) {
     }
     
     if (m.isGroup) {
-        let pp = 'https://telegra.ph/file/9243544e7ab350ce747d7.mp4'; 
-        let pp2 = 'https://telegra.ph/file/fadc180ae9c212e2bd3e1.mp4'; 
-        let pp3 = 'https://telegra.ph/file/79a5a0042dd8c44754942.mp4';
-        let pp4 = 'https://telegra.ph/file/035e84b8767a9f1ac070b.mp4';
-        let pp5 = 'https://telegra.ph/file/0103144b636efcbdc069b.mp4';
-        let pp6 = 'https://telegra.ph/file/4d97457142dff96a3f382.mp4';
-        let pp7 = 'https://telegra.ph/file/b1b4c9f48eaae4a79ae0e.mp4';
-        let pp8 = 'https://telegra.ph/file/5094ac53709aa11683a54.mp4';
-        let pp9 = 'https://telegra.ph/file/5094ac53709aa11683a54.mp4';
-        let pp10 = 'https://telegra.ph/file/dc279553e1ccfec6783f3.mp4';
-        let pp11 = 'https://telegra.ph/file/acdb5c2703ee8390aaf33.mp4';
+        let pp = 'https://files.catbox.moe/p4a3h0.gif'; 
+        let pp2 = 'https://files.catbox.moe/sdrjkn.jpeg'; 
+        let pp3 = 'https://files.catbox.moe/3y1hsw.jpeg';
+        let pp4 = 'https://files.catbox.moe/8zznyi.jpeg';
+        let pp5 = 'https://files.catbox.moe/067zfv.gif';
+        let pp6 = 'https://files.catbox.moe/rtu9do.gif';
+        let pp7 = 'https://files.catbox.moe/vp42zo.gif';
+        let pp8 = 'https://files.catbox.moe/ukq9n2.gif';
+        let pp9 = 'https://files.catbox.moe/jsma1w.gif';
+        let pp10 = 'https://files.catbox.moe/l4nnpo.gif';
+        let pp11 = 'https://files.catbox.moe/0h835p.gif';
         
         const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11];
         const video = videos[Math.floor(Math.random() * videos.length)];
